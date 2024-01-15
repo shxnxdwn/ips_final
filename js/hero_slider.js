a8ac05e66869b85a1cd02a83663618eadb15c1f7
Глеб <esplete@gmail.com>
2024-01-16 02:17:44 +0300

    Add hero slider

---
+++ js/hero_slider.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const heroSlider = document.querySelector('.hero__slider');
+const images = heroSlider.querySelectorAll('img');
+
+const sliderActiveImage = 'img/hero/slider-active.svg';
+const sliderInactiveImage = 'img/hero/slider-inactive.svg';
+
+let count = 0;
+
+const changeImages = () => {
+    images.forEach((image) => {
+        image.src = sliderInactiveImage;
+    });
+    images[count].src = sliderActiveImage;
+    count = (count + 1) % images.length;
+    setTimeout(changeImages, 3000);
+};
+
+changeImages();
