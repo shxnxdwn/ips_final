6196545fe0e3ff0d2bce9b09f28ccde6ea517108
Глеб <esplete@gmail.com>
2024-01-15 21:49:44 +0300

    Add animation for burger

--- js/script.js
+++ js/script.js
@@ -6,12 +6,7 @@ const menu = document.querySelector('.nav__list');
 burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     menu.classList.toggle('open');
-
-    if (document.body.style.overflow === 'hidden') {
-        document.body.style.overflow = '';
-    } else {
-        document.body.style.overflow = 'hidden';
-    }
+    document.body.classList.toggle('noScroll');
 });
 
 
