5eea7b89606536f0bd395e7567925297892923b1
Глеб <esplete@gmail.com>
2024-01-15 20:50:58 +0300

    Add hovers

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 "use strict";
 
+const burger = document.querySelector('.nav__burger-button');
+const menu = document.querySelector('.nav__list');
 
+burger.addEventListener('click', () => {
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
 
-
-
-
+    if (document.body.style.overflow === 'hidden') {
+        document.body.style.overflow = '';
+    } else {
+        document.body.style.overflow = 'hidden';
+    }
+});
 
 
 
