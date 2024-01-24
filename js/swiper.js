c7de9d9bfa8d966f3a6c9460b5f41de2926378c9
Глеб <esplete@gmail.com>
2024-01-24 20:44:57 +0300

    fix bugs

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const swiper = new Swiper('.projects__row', {
+const swiper = new Swiper('.swiper', {
     navigation: {
         nextEl: ".projects__button_next",
         prevEl: ".projects__button_prev",
@@ -8,18 +8,28 @@ const swiper = new Swiper('.projects__row', {
     autoplay: {
         delay: 3000,
     },
+    slidesPerView: 4,
+    slidesPerGroup: 4,
     breakpoints: {
-        1920: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
-        },
-        768: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-        },
         576: {
             slidesPerView: 1,
             slidesPerGroup: 1,
+            spaceBetween: 0,
+        },
+        1440: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 10,
+        },
+        1600: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 170,
+        },
+        1800: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 30,
         },
     },
 });
